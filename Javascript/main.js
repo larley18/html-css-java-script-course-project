93f1acecaf769b2c6fabdebfa41b41bf4b9f6b7e
larley18 <111449935+larley18@users.noreply.github.com>
2023-02-12 10:45:33 +0000

    new course
    still learning a new course

---
+++ Javascript/main.js
@@ -0,0 +1,7 @@
+
+var welcome_message=("this is a welcome message stored inside a variable");
+alert( welcome_message);
+console.log("this is a log")
+var myAge=28;
+var exactAge = 28.6;
+console.log(myAge+exactAge)
