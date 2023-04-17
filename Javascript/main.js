30a0d1c9aa9227dc39a0997498783c6617a7173e
larley18 <111449935+larley18@users.noreply.github.com>
2023-04-17 15:48:08 +0000

    changes
    changes

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,18 +1,21 @@
 
-var welcome_message=("this is a welcome message stored inside a variable");
-alert( welcome_message);
-console.log("this is a log")
-var myAge=28;
-if(myAge<1){console.log("you are baby")} else if (myAge < 10){console.log( "you are a big kid")} else if (myage<10) 
-{console.log("you're a big kid")}
+// var welcome_message=("this is a welcome message stored inside a variable");
+// alert( welcome_message);
+// console.log("this is a log")
+// var myAge=28;
+// if(myAge<1){console.log("you are baby")} else if (myAge < 10){console.log( "you are a big kid")} else if (myage<10) 
+// {console.log("you're a big kid")}
+
+var btn = document.getElementById("go-button");
 
 function buttonclicked(){
-    console.log("Button Clicked"),
+    console.log("Button Clicked");
+    btn.removeEventListener("click",buttonclicked);
+    document.getElementById("text").innerHTML="Dont do it!";
 }
 
-var btn = document.getElementById(go-button)
 
-btn.addEventListener("click",button clicked)
+btn.addEventListener("click",buttonclicked);
 
 
  
